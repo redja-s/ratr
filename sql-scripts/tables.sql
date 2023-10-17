a5859e14501875f1bde986dc4d207c835a9f1392
jared <jared_solano@protonmail.com>
2023-10-17 21:34:57 +0100

    rename folders

--- sql-scripts/tables.sql
+++ sql-scripts/tables.sql
@@ -2,8 +2,6 @@ CREATE TABLE IF NOT EXISTS films (
     id uuid,
     title VARCHAR NOT NULL,
     description VARCHAR NOT NULL,
-    genre genre_enum NOT NULL,
     director uuid NOT NULL,
-    director_name VARCHAR NOT NULL,
-
-)
+    director_name VARCHAR NOT NULL
+);
