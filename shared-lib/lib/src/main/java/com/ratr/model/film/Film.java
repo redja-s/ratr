94f442cae5b52e5d0d6955b70102c59052e2bfad
jared <jared_solano@protonmail.com>
2023-11-03 21:10:41 +0000

    re-enable genre model

--- shared-lib/lib/src/main/java/com/ratr/model/film/Film.java
+++ shared-lib/lib/src/main/java/com/ratr/model/film/Film.java
@@ -7,8 +7,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- shared-lib/lib/src/main/java/com/ratr/model/film/Genre.java
+++ shared-lib/lib/src/main/java/com/ratr/model/film/Genre.java
@@ -1,22 +1,21 @@
 package com.ratr.model.film;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
-//@Entity
-//@Table(name = "genres")
-//@Builder
-//public class Genre {
-//    @Id
-//    @GeneratedValue
-//    private int id;
-//
-//    @NotNull
-//    private String genre;
-//}
+@Entity
+@Table(name = "genres")
+@Builder
+public class Genre {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    private String genre;
+}
