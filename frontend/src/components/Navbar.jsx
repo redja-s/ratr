148f5ae6c6e4df74ec2adf8cae90a680768c8c91
jared <jtlss14@gmail.com>
2024-06-13 21:56:01 +0100

    cleanup nav bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,44 +1,36 @@
 import RatrLogo from '../logo.svg';
 import { NavLink } from 'react-router-dom';
-import DarkModeToggle from "./DarkModeToggle.jsx";
 import "../css/Navbar.css";
 import { IoHome } from "react-icons/io5";
 import { IoFilm } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <nav className="w-full fixed bg-white dark:bg-black items-center flex-wrap">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between">
-        <div className="flex items-center flex-wrap space-x-5">
-          <a href="/" className="flex items-center flex-wrap space-x-2">
-            <img src={RatrLogo} className="h-8" alt="ratr" />
-            <span className="font-extrabold px-0 mx-0 dark:text-white">
-              ratr
-            </span>
-          </a>
-        </div>
-        <div className="justify-center border-red-600 hidden w-full bg-black dark:bg-black md:block md:w-auto" id="navbar-default">
-          <ul className="flex flex-col md:p-0 font-bold mt-4 bg-black md:flex-row md:space-x-4 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-black md:dark:bg-black dark:text-white">
-            <li className="navbar-list-item">
-              <DarkModeToggle />
-            </li>
-            <li className="navbar-list-item">
-              <NavLink to="/">
-                <IoHome size={20} />
-              </NavLink>
-            </li>
-            <li className="navbar-list-item">
-              <NavLink to="/films">
-                <IoFilm size={20} />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/login" className="flex items-center p-2 rounded-lg bg-blue-400 hover:bg-blue-500 text-white dark:text-white">
-                Sign In
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+    <nav className="bg-black text-white text-sm">
+      <div className="flex items-center flex-wrap py-4 pl-6">
+        <a href="/" className="flex items-center flex-wrap pr-4 space-x-2">
+          <img src={RatrLogo} className="h-8" alt="ratr" />
+          <span>
+            ratr
+          </span>
+        </a>
+        <ul className="flex pl-5 items-center space-x-6 border-l border-slate-200">
+          <li className="navbar-list-item">
+            <NavLink to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="navbar-list-item">
+            <NavLink to="/films">
+              Films
+            </NavLink>
+          </li>
+          <li className="navbar-list-item">
+            <NavLink to="/login" className="p-1 border border-white">
+              Sign In
+            </NavLink>
+          </li>
+        </ul>
       </div>
     </nav>
   );
